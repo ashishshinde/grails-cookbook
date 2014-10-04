ee8ec17e348dedfe952ebf23ab4540fbbf1442ec
ashish <ashish@strandls.com>
2014-10-04 19:42:53 +0530

    Fixed issues with name of cookbook missing

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,6 @@ license          "All rights reserved"
 description      "Installs/Configures grails"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+name             "grails-cookbook"
 depends "java"
 depends "ark"
